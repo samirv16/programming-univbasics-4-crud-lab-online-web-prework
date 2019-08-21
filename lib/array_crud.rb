fd8b860f068dc48187e61437fd21a91f1137d022
edras vallecillo <samir.vallecillo@outlook.com>
2019-08-21 05:48:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -25,7 +25,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  cities = ["San Diego", "Des Moines", "Omaha"]
+  cities = ["San Diego", "Des Moines", "am"]
   cities[2]
 end
 
