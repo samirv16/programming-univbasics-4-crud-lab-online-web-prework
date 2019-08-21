210ca7bd2bf77f6f84cbc22d3e9913f68d5d1229
edras vallecillo <samir.vallecillo@outlook.com>
2019-08-21 05:50:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -30,8 +30,8 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  cities = ["San Diego", "Des Moines", "am"]
-  cities[0]
+  array = []
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
